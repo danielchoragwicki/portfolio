eca11033ae1091c9a836c8a4f33f6d051e958075
Daniel Chorągwicki <choragwickid@gmail.com>
2018-08-02 17:08:43 +0200

    Menu 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import './main.css';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
+import Header from './components/Header'
+import './main.css'
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <Router>
+        <Fragment>
+          <Header />
+            <Route exact path='/' render={props => (<div>Daniel Chorągwicki</div>)}/>
+            <Route path="/o-mnie" render={props => (<div className="asdasd">o mnie</div>)}/>
+            <Route path="/portfolio" render={props => (<div>p</div>)}/>
+            <Route path="/kontakt" render={props => (<div>k</div>)}/>
+            <Redirect to="/"/>
+          </Fragment>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import { Link, NavLink } from "react-router-dom"
 import { Transition, Keyframes, animated, config } from 'react-spring'
 import delay from 'delay'
-import MediaQuery from 'react-responsive';
+import MediaQuery from 'react-responsive'
  
 
 export default class Header extends Component {
@@ -17,7 +18,7 @@ export default class Header extends Component {
         const { isOpen } = this.state;
         return (
             <header className="header">
-                    <a href="#" className={`logo${ isOpen ? ' logo--active' : ''}`}>daniel chorągwicki</a>
+                    <Link to="/" className={`logo${ isOpen ? ' logo--active' : ''}`}>Daniel Chorągwicki</Link>
                 <MediaQuery maxDeviceWidth={768}>
                     <button className={`menu-button${ isOpen ? ' menu-button--active' : ''}` } onClick={this.handleClick}>
                         <div className="menu-button__line"></div>
@@ -27,9 +28,15 @@ export default class Header extends Component {
                 </MediaQuery>
                 <nav className={`menu${ isOpen ? ' menu--open' : ''}`}>
                     <ul className="menu__list">
-                        <li className="menu__item"><a href="#" className="menu__link">O mnie</a></li>
-                        <li className="menu__item"><a href="#" className="menu__link" >Portfolio</a></li>
-                        <li className="menu__item"><a href="#" className="menu__link menu__link--active">Kontakt</a></li>
+                        <li className="menu__item">
+                            <NavLink to="/o-mnie" className="menu__link" activeClassName="menu__link--active">O mnie</NavLink>
+                        </li>
+                        <li className="menu__item">
+                            <NavLink to="/portfolio" className="menu__link" activeClassName="menu__link--active">Portfolio</NavLink>
+                        </li>
+                        <li className="menu__item">
+                            <NavLink to="/kontakt" className="menu__link" activeClassName="menu__link--active">Kontakt</NavLink>
+                        </li>
                     </ul>
                 </nav>
             </header>
